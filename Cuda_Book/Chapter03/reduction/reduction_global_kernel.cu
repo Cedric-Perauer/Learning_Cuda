44e5c08385ee8f1684aabf85241aa4afe194e7cd
Cedric-Perauer <ceddyperauer@gmail.com>
2020-10-05 00:29:00 +0200

    add reduce shared

--- Cuda_Book/Chapter03/reduction/reduction_global_kernel.cu
+++ Cuda_Book/Chapter03/reduction/reduction_global_kernel.cu
@@ -2,10 +2,10 @@
 #include <stdlib.h> 
 
 __global__ void global_reduction_kernel(float *data_out, float *data_in, int stride, int size) { 
-	int idx_x = threadIdx.x + blockIdx.x * blockDim.x 
+	int idx_x = threadIdx.x + blockIdx.x * blockDim.x; 
 
 	if(idx_x + stride < size) { 
-	data_out[idx_x] += data_in[idx_x + stride] //same at every step only stride differs 
+	data_out[idx_x] += data_in[idx_x + stride]; //same at every step only stride differs 
 	} 
 } 
 
@@ -13,7 +13,7 @@ __global__ void global_reduction_kernel(float *data_out, float *data_in, int str
 void global_reduction(float *d_out, float *d_in, int n_threads, int size) { 
 int blocks_n = (size + n_threads - 1) /n_threads; 
 
-for(int s = 1; stride < size; stride *= 2) { 
+for(int stride = 1; stride < size; stride *= 2) { 
 	global_reduction_kernel<<<blocks_n,n_threads>>>(d_out,d_in,stride,size); 
 } 
 }  
--- Cuda_Book/Chapter03/reduction/reduction_shared.cpp
+++ Cuda_Book/Chapter03/reduction/reduction_shared.cpp
@@ -7,9 +7,9 @@
 
 #include "reduction.h"
 
-void run_benchmark(void (*reduce)(float *, float *, int, int),
+void run_benchmark(void (*reduce)(float*, float*, int, int), 
                    float *d_outPtr, float *d_inPtr, int size);
-void init_input(float *data, int size);
+void init_input(float* data, int size);
 float get_cpu_result(float *data, int size);
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -27,19 +27,19 @@ int main(int argc, char *argv[])
     srand(2019);
 
     // Allocate memory
-    h_inPtr = (float *)malloc(size * sizeof(float));
-
+    h_inPtr = (float*)malloc(size * sizeof(float));
+    
     // Data initialization with random values
     init_input(h_inPtr, size);
 
     // Prepare GPU resource
-    cudaMalloc((void **)&d_inPtr, size * sizeof(float));
-    cudaMalloc((void **)&d_outPtr, size * sizeof(float));
+    cudaMalloc((void**)& d_inPtr, size * sizeof(float));
+    cudaMalloc((void**)&d_outPtr, size * sizeof(float));
 
     cudaMemcpy(d_inPtr, h_inPtr, size * sizeof(float), cudaMemcpyHostToDevice);
 
     // Get reduction result from GPU
-    run_benchmark(global_reduction, d_outPtr, d_inPtr, size);
+    run_benchmark(reduction, d_outPtr, d_inPtr, size);
     cudaMemcpy(&result_gpu, &d_outPtr[0], sizeof(float), cudaMemcpyDeviceToHost);
 
     // Get reduction result from GPU
@@ -47,7 +47,7 @@ int main(int argc, char *argv[])
     // Get all sum from CPU
     result_host = get_cpu_result(h_inPtr, size);
     printf("host: %f, device %f\n", result_host, result_gpu);
-
+    
     // Terminates memory
     cudaFree(d_outPtr);
     cudaFree(d_inPtr);
@@ -56,8 +56,8 @@ int main(int argc, char *argv[])
     return 0;
 }
 
-void run_benchmark(void (*reduce)(float *, float *, int, int),
-                   float *d_outPtr, float *d_inPtr, int size)
+void run_benchmark(void (*reduce)(float*, float*, int, int), 
+              float *d_outPtr, float *d_inPtr, int size)
 {
     int num_threads = 256;
     int test_iter = 100;
@@ -73,11 +73,8 @@ void run_benchmark(void (*reduce)(float *, float *, int, int),
     ////////
     // Operation body
     ////////
-    for (int i = 0; i < test_iter; i++)
-    {
-        cudaMemcpy(d_outPtr, d_inPtr, size * sizeof(float), cudaMemcpyDeviceToDevice);
-        reduce(d_outPtr, d_outPtr, num_threads, size);
-        cudaDeviceSynchronize();
+    for (int i = 0; i < test_iter; i++) {
+        reduce(d_outPtr, d_inPtr, num_threads, size);
     }
 
     // getting elapsed time
