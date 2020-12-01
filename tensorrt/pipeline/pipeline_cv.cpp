e2d2d66816c06c769490f3a8b3cf6a1665133d85
cepefz00 <cedricperauer@gmail.com>
2020-12-01 19:41:54 +0100

    rektnet single batch

--- tensorrt/pipeline/pipeline_cv.cpp
+++ tensorrt/pipeline/pipeline_cv.cpp
@@ -8,23 +8,28 @@
 #include <torch/torch.h>
 
 
-torch::Tensor soft_argmax(OUT input){ 
+torch::Tensor flat_softmax(OUT input) {
 
 auto options = torch::TensorOptions().dtype(torch::kFloat32).device(torch::kCPU,0);
-
-auto tharray = torch::zeros((1,7,80,80),torch::kFloat32); //or use kF64
-std::memcpy(tharray.data_ptr(),input,sizeof(float)*tharray.numel());
 torch::Tensor values_x = torch::linspace(0,(REKT_SIZE -1.0)/REKT_SIZE, REKT_SIZE, options); 
 torch::Tensor values_y = torch::linspace(0,(REKT_SIZE -1.0)/REKT_SIZE, REKT_SIZE, options); 
+auto tharray = torch::zeros({1,7,80,80},torch::kFloat32); //or use kF64
 
-std::cout << "shapes" << tharray.sizes()  << std::endl;
-//torch::Tensor exp_y = (tharray.sum(2) * values_y).sum(-1); 
-//torch::Tensor exp_x = (tharray.sum(2) * values_x).sum(-1); 
+auto start = std::chrono::system_clock::now();
+//convert array to torch tensor 
+std::memcpy(tharray.data_ptr(),input,sizeof(float)*tharray.numel());
+torch::Tensor flat = tharray.view({-1,REKT_SIZE*REKT_SIZE}); 
+flat = torch::nn::functional::softmax(flat,1);
+torch::Tensor hm = flat.view({-1,7,REKT_SIZE,REKT_SIZE}); 
+torch::Tensor exp_y = (hm.sum(3) * values_y).sum(-1); 
+torch::Tensor exp_x = (hm.sum(2) * values_x).sum(-1); 
 
-//return torch::stack({exp_x,exp_y});
-return values_x; 
-} 
 
+auto end = std::chrono::system_clock::now();
+    std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << "ms" << std::endl;
+
+return torch::stack({exp_x, exp_y},-1); 
+}
 
 
 
@@ -35,11 +40,15 @@ int main(int argc, char** argv) {
      Rektnet rektnet = Rektnet(1);
      
       auto out_rekt = rektnet.inference("samples_rekt/cut.jpg");  
-       auto start = std::chrono::system_clock::now();
-      torch::Tensor out = soft_argmax(out_rekt); 
- auto end = std::chrono::system_clock::now();
+      auto start = std::chrono::system_clock::now();
+      auto out = flat_softmax(out_rekt); 
+     std::cout << "size" << out.sizes() << std::endl; 
+      //torch::Tensor out = soft_argmax(out_rekt); 
+      std::cout << out << std::endl;
+     auto end = std::chrono::system_clock::now();
     std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << "ms" << std::endl;    
- return 0;
+      
+    return 0;
 }
 
 
