3c3ab5eee956cc0fd0f66d989200b6096f81944d
cepefz00 <cedricperauer@gmail.com>
2020-12-07 02:18:58 +0100

    torch version progress

--- tensorrt/pipeline/rektnet_torch.cpp
+++ tensorrt/pipeline/rektnet_torch.cpp
@@ -22,7 +22,7 @@ struct Resnet: torch::nn::Module {
 		shortcut_conv(register_module("shortcut_conv",torch::nn::Conv2d(in_channels,out_channels,1,1)));
                 relu(register_module("relu",torch::nn::ReLU())); 
 		
-	}
+		{}
         
         torch::Tensor forward(torch::Tensor x) {
                       torch::Tensor x_forward; 
@@ -36,5 +36,81 @@ struct Resnet: torch::nn::Module {
  
 
 
-}
+};
 
+
+
+struct Resnet_C_Block: torch::nn::Module {
+        torch::nn::Conv2d conv1, conv2,conv3;
+        torch::nn::BatchNorm2d bn1,bn2,bn3;
+        torch::nn::ReLU relu;
+        torch::nn::LeakyReLU leaky_relu;
+
+
+
+
+
+        Resnet(in_channels, out_channels) :
+
+
+                conv1(register_module("conv1",torch::nn::Conv2d(in_channels,out_channels/4.0,3,1)));
+                conv2(register_module("conv2",torch::nn::Conv2d(out_channels/4.0,out_channels/2.0,3,1)));
+                conv3(register_module("conv3",torch::nn::Conv2d(out_channels/2.0,out_channels,3,1)));
+                bn1(register_module("bn1", torch::nn::BatchNorm2d(out_channels/4.0)));
+                bn2(register_module("bn2", torch::nn::BatchNorm2d(out_channels/2.0)));
+                bn3(register_module("bn3", torch::nn::BatchNorm2d(out_channels)));
+                relu(register_module("relu",torch::nn::ReLU()));
+
+                {}
+
+        torch::Tensor forward(torch::Tensor x) {
+                      x = relu(bn1(conv1(x)));
+                      x = relu(bn2(conv2(x)));
+                      x = relu(bn3(conv3(x)));
+
+
+                      return x;
+        }
+
+
+
+};
+
+
+
+
+class KeypointNet : torch::nn::Module {
+              Resnet res1,res2,res3,res4; 
+              Resnet_C_Block res_c; 
+              torch::nn::BatchNorm2d bn;
+	      torch::nn::Conv2d out; 
+              torch::nn::ReLU relu;
+	      int net_size = 16;       
+	      int num_kpt = 7; 
+	   
+      KeypointNet() :
+          
+                res1(register_module("res1",Resnet(net_size,net_size)));
+                res2(register_module("res2",Resnet(net_size,2*net_size)));
+                res3(register_module("res3",Resnet(net_size*2,4*net_size)));
+                res4(register_module("res4",Resnet(net_size*4,8*net_size)));
+                
+                bn(register_module("bn", torch::nn::BatchNorm2d(net_size)));
+                res_c(register_module("res_c",Resnet_C_Block(3,net_size)));
+		out(register_module("out",torch::nn::Conv2d(8 * net_size,num_kpt,1,1,0)));
+                 
+                relu(register_module("relu",torch::nn::ReLU()));
+                 {}
+
+     torch::Tensor forward(torch::Tensor x) {
+       x = relu(bn(res_c(x)));
+       x = res1(x); 
+       x = res2(x); 
+       x = res3(x); 
+       x = res4(x);
+       x = out(x);  
+
+
+     }
+      
+};
