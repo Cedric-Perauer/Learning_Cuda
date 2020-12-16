877850221bbdc4ea588810b4fc05704385da4972
Cedric Perauer <40869163+Cedric-Perauer@users.noreply.github.com>
2020-12-16 19:14:13 +0100

    Add files via upload

--- tensorrt/pipeline/pipeline_cv.cpp
+++ tensorrt/pipeline/pipeline_cv.cpp
@@ -8,14 +8,26 @@
 #include "rektnet.cpp"
 #include <torch/torch.h>
 
+class Pipeline { 
+
+torch::Tensor values_x; 
+torch::Tensor values_y; 
+torch::Tensor tharray; 
+
+public : 
+
+Pipeline(){ 
+auto options = torch::TensorOptions().dtype(torch::kFloat32).device(torch::kCPU,0);
+values_x = torch::linspace(0,(REKT_SIZE -1.0)/REKT_SIZE, REKT_SIZE, options); 
+values_y = torch::linspace(0,(REKT_SIZE -1.0)/REKT_SIZE, REKT_SIZE, options); 
+tharray = torch::zeros({10,7,80,80},torch::kFloat32); //or use kF64
+
+} 
+
+~Pipeline(){}
 
 torch::Tensor flat_softmax(OUT input) {
 
-auto start1 = std::chrono::system_clock::now(); 
-auto options = torch::TensorOptions().dtype(torch::kFloat32).device(torch::kCPU,0);
-torch::Tensor values_x = torch::linspace(0,(REKT_SIZE -1.0)/REKT_SIZE, REKT_SIZE, options); 
-torch::Tensor values_y = torch::linspace(0,(REKT_SIZE -1.0)/REKT_SIZE, REKT_SIZE, options); 
-auto tharray = torch::zeros({10,7,80,80},torch::kFloat32); //or use kF64
 
 auto start = std::chrono::system_clock::now();
 //convert array to torch tensor 
@@ -29,29 +41,62 @@ torch::Tensor exp_x = (hm.sum(2) * values_x).sum(-1);
 
 auto end = std::chrono::system_clock::now();
     std::cout << "Softmax " << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << "ms" << std::endl;
-    std::cout << "Softmax pre process " << std::chrono::duration_cast<std::chrono::milliseconds>(end - start1).count() << "ms" << std::endl;
 
 return torch::stack({exp_x, exp_y},-1); 
+} 
+
+void plot_pts(std::vector<Yolo::Detection>& res, std::vector<std::vector<float>> &bbox_vals, torch::Tensor &rektnet)
+{
+
+cv::Mat img2 = cv::imread("/home/pjfsd/Learning_Cuda/tensorrt/pipeline/samples/img1.jpg");
+for(size_t i=0; i < res.size();i++)
+{ 
+          cv::Rect r = get_rect(img2, res[i].bbox);
+	  cv::rectangle(img2, r, cv::Scalar(0x27, 0xC1, 0x36), 2);
+          cv::putText(img2, std::to_string((int)res[i].class_id), cv::Point(r.x, r.y - 1), cv::FONT_HERSHEY_PLAIN, 1.2, cv::Scalar(0xFF, 0xFF, 0xFF), 2);
+	  
+	  for(int j = 0; j < 7 ;++j)
+	  {    
+		  std::vector<float> v(rektnet[i][j].data_ptr<float>(), rektnet[i][j].data_ptr<float>() + rektnet[i][j].numel());
+		  cv::circle(img2,cv::Point(r.x+int(v[0] * r.width), r.y + int(v[1] * r.height)),2,cv::Scalar(255,0,0),1);  
+
+	  }
+
+
+}	
+
+cv::imshow("circle",img2); 
+cv::waitKey(0); 
+
+
 }
 
 
+}; 
 
-int main(int argc, char** argv) {
+
+
+int main(int argc, char** argv) 
+{
      
      //inits
      YOLO_INF yolov5 = YOLO_INF();
      Rektnet rektnet = Rektnet(10);
+     Pipeline pipeline = Pipeline(); 
+     
      std::vector<cv::Mat> imgs = yolov5.inference("samples/img1.jpg",1); 
-     for(int i= 0; i < 100; ++i)
+     for(int i= 0; i < 1; ++i)
      { 
      auto start = std::chrono::system_clock::now();
 
      imgs = yolov5.inference("samples/img1.jpg",1); 
       
      auto out_rekt = rektnet.inference(imgs);  
-     auto out = flat_softmax(out_rekt); 
+     auto out = pipeline.flat_softmax(out_rekt); 
      auto end = std::chrono::system_clock::now();
+     std::cout << out << std::endl; 
     std::cout << "total time :" << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << "ms" << std::endl;
+     pipeline.plot_pts(yolov5.res,yolov5.box_coords,out ); 
      }
      return 0;
 }
